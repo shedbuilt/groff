35888a50b3ff7f97f68125cae634bef6ec88c9d2
Auston Stewart <auston@shedbuilt.net>
2018-06-04 16:17:07 -1000

    Adding a docs option

--- build.sh
+++ build.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
-PAGE=letter ./configure --prefix=/usr &&
+declare -A SHED_PKG_LOCAL_OPTIONS=${SHED_PKG_OPTIONS_ASSOC}
+PAGE=letter \
+./configure --prefix=/usr \
+            --docdir="$SHED_PKG_DOCS_INSTALL_DIR" &&
 # Explicitly prevent parallel builds
 make -j1 &&
-make DESTDIR="$SHED_FAKE_ROOT" install
+make DESTDIR="$SHED_FAKE_ROOT" install || exit 1
+# Prune Documentation
+if [ -z "${SHED_PKG_LOCAL_OPTIONS[docs]}" ]; then
+    rm -rf "${SHED_FAKE_ROOT}${SHED_PKG_DOCS_INSTALL_DIR}"
+fi
